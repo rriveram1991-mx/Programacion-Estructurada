42416a6627c893d76cb7941464e04e00638d6aaa
Vellito34 <151584119+Vellito34@users.noreply.github.com>
2026-02-24 10:32:50 -0600

    Add files via upload

---
+++ 2E TSU/Semana 4/Luis Alejandro Galvan Gonzalez/SistemaEvaluacionSIS.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class SistemaEvaluacionSIS {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion = 0;
+        int total = 0;
+        int contador = 0;
+
+        System.out.println("=== SISTEMA DE EVALUACION ===");
+
+        while (opcion != 4) {
+
+            System.out.println("1. Capturar calificaciones");
+            System.out.println("2. Mostrar promedio");
+            System.out.println("3. Mostrar nivel");
+            System.out.println("4. Salir");
+
+            System.out.print("Seleccione opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    for (int i = 1; i <= 5; i++) {
+                        System.out.print("Ingrese calificacion " + i + ": ");
+                        int cal = sc.nextInt();
+
+                        if (cal >= 0 && cal <= 10){
+                            total = total + cal;
+                            contador++;
+                        }else{
+                            System.out.println("Calificacion invalida");}}
+                    break;
+
+                case 2:
+                    if (contador > 0) {
+                        double promedio = (double) total / contador;
+                        System.out.println("Promedio: " + promedio);
+                    } else {
+                        System.out.println("No hay calificaciones capturadas");
+                    }
+                    break;
+
+                case 3:
+                    if (contador > 0) {
+                        double promedio = (double) total / contador;
+                        if (promedio >= 9) {
+                            System.out.println("Excelente");
+                        } else if (promedio >= 8) {
+                            System.out.println("Bueno");
+                        } else if (promedio >= 7) {
+                            System.out.println("Regular");
+                        } else {
+                            System.out.println("Reprobado");
+                        }
+                    } else {
+                        System.out.println("No hay calificaciones capturadas");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+
+        }
+
+        sc.close();
+    }
+}
