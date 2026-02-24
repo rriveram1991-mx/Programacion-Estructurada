bafbf16de0494e033606c4fdbc34730a4b5d3878
Rod145 <rodrichavez145@gmail.com>
2026-02-24 14:26:47 -0600

    Add files via upload

---
+++ 2E TSU/Semana 4/Rodrigo Chávez Compean/ejercicio2CorrecionErrores24022026/JavaApplication25.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author rodri
+ */
+import java.util.Scanner;
+
+public class JavaApplication25 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+        double total = 0;
+        double contador = 0;
+
+        System.out.println("=== SISTEMA DE EVALUACION ===");
+
+        while (opcion != 4) {
+
+            System.out.println("1. Capturar calificaciones");
+            System.out.println("2. Mostrar promedio");
+            System.out.println("3. Mostrar nivel");
+            System.out.println("4. Salir");
+
+            System.out.print("Seleccione opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    for (int i = 1; i <= 5; i++) {
+                        System.out.print("Ingrese calificacion " + i + ": ");
+                        int cal = sc.nextInt();
+
+                        if (cal >= 0 && cal <= 10){
+                            total += cal;
+                            contador++;}
+                            else 
+                            System.out.println("Calificacion invalida");
+                    }
+                    break;
+
+                case 2:
+                    double promedio = total / contador;
+                    System.out.println("Promedio: " + promedio);
+                    break;
+
+                case 3:
+                    if (total / contador >= 9)
+                        System.out.println("Excelente");
+                    else if (total / contador >= 8)
+                        System.out.println("Bueno");
+                    else if (total / contador >= 7)
+                        System.out.println("Regular");
+                    else
+                        System.out.println("Reprobado");
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+
+        }
+
+        sc.close();
+    }
+}
