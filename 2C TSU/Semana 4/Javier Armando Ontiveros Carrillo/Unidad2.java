c9a10ee3ca8cb428ae0748cb7c3d60ef046eac9f
Vixine4501 <javiers619n@gmail.com>
2026-02-24 10:07:22 -0600

    Add files via upload

---
+++ 2C TSU/Semana 4/Javier Armando Ontiveros Carrillo/Unidad2.java
@@ -0,0 +1,101 @@
+package unidad.pkg2;
+import java.util.Scanner;
+
+public class Unidad2 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+        int total = 0;
+        int cantidad = 0;
+        boolean salir = false;
+
+        System.out.println("=== SISTEMA DE CONTROL DE NOTAS ===");
+
+        while (!salir) {
+
+            System.out.println("1. Capturar notas");
+            System.out.println("2. Mostrar promedio");
+            System.out.println("3. Mostrar estadisticas");
+            System.out.println("4. Reiniciar datos");
+            System.out.println("5. Salir");
+
+            System.out.print("Seleccione opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.print("Cuantas notas desea capturar: ");
+                    int n = sc.nextInt();
+
+                    for (int i = 1; i <= n; i++) {
+                        System.out.print("Ingrese nota #" + i + ": ");
+                        int nota = sc.nextInt();
+
+                        if (nota >= 0 && nota <= 10) {
+                            total += nota;
+                            cantidad++;
+                        } else {
+                            System.out.println("Nota invalida");
+                        }
+                    }
+                    break;
+
+                case 2:
+                    if (cantidad == 0) {
+                        System.out.println("No hay notas registradas");
+                        break;
+                    }
+
+                    double promedio = (double) total / cantidad;
+                    System.out.println("Promedio: " + promedio);
+
+                    if (promedio >= 9)
+                        System.out.println("Excelente");
+                    else if (promedio >= 8)
+                        System.out.println("Bueno");
+                    else if (promedio >= 7)
+                        System.out.println("Regular");
+                    else
+                        System.out.println("Reprobado");
+                    break;
+
+                case 3:
+                    if (cantidad == 0) {
+                        System.out.println("No hay datos para estadisticas");
+                        break;
+                    }
+
+                    int aprobados = 0;
+                    int reprobados = 0;
+
+                    double promedioFinal = (double) total / cantidad;
+
+                    if (promedioFinal >= 7)
+                        aprobados = cantidad;
+                    else
+                        reprobados = cantidad;
+
+                    System.out.println("Aprobados: " + aprobados);
+                    System.out.println("Reprobados: " + reprobados);
+                    break;
+
+                case 4:
+                    total = 0;
+                    cantidad = 0;
+                    System.out.println("Datos reiniciados");
+                    break;
+
+                case 5:
+                    salir = true;
+                    System.out.println("Saliendo del sistema");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+    }
+}
