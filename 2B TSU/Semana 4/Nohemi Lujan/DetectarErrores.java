53098a87209ceebdae750e7e4edd5eafd6cda9a9
n4lr-24 <nohemilujan1@gmail.com>
2026-02-23 22:42:28 -0600

    Create DetectarErrores.java

---
+++ 2B TSU/Semana 4/Nohemi Lujan/DetectarErrores.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package detectar.errores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nohem
+ */
+public class DetectarErrores.java {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int opcion=0;
+        double total=0;
+        int cantidad=0;
+        boolean salir = false;
+        int nota=0;
+        int aprobados=0, reprobados=0;
+        System.out.println("=== SISTEMA DE CONTROL DE NOTAS ===");
+
+        while (salir == false) { 
+
+            System.out.println("\n1. Capturar notas");
+            System.out.println("2. Mostrar promedio");
+            System.out.println("3. Mostrar estadisticas");
+            System.out.println("4. Reiniciar datos");
+            System.out.println("5. Salir");
+
+            System.out.print("Seleccione opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+
+                    System.out.print("Cuantas notas desea capturar: ");
+                    int n = sc.nextInt();
+
+                    for (int i = 1; i <= n; i++) {
+
+                        System.out.print("Ingrese nota #" + i + ": ");
+                        nota = sc.nextInt();
+                    
+                
+                    if (nota >=0 && nota<= 10) {
+                        total += nota;
+                        cantidad++;
+                        if (nota>=7){
+                            aprobados++;
+                        }else{
+                            reprobados++;
+                        }
+                        
+                    }else{
+                         
+                        System.out.println("Nota invalida");
+                        i--;
+                    }
+                
+                    }             
+                    break;
+
+                case 2:
+                    if (cantidad==0){
+                        System.out.println("no hay notas registradas");
+                    } else {
+                    double promedio =  total / cantidad;
+                    System.out.println("Promedio: " + promedio);
+
+                    if (promedio >= 9)
+                        System.out.println("Excelente");
+                    else if (promedio >= 8)
+                        System.out.println("Bueno");
+                    else if (promedio >= 7)
+                        System.out.println("Regular");
+                    else
+                        System.out.println("Reprobado");
+                          } 
+                    break;
+                    
+                case 3:
+                    if (cantidad==0){
+                        System.out.println("sin datos");
+                    }else{
+                    
+                    System.out.println("Aprobados: " + aprobados);
+                    System.out.println("Reprobados: " + reprobados);
+                    break;
+                    }
+                case 4:
+
+                    total = 0;
+                    cantidad = 0;
+                    aprobados=0;
+                    reprobados=0;
+                    System.out.println("Datos reiniciados");
+                  break;
+                case 5:
+
+                    salir = true;
+                    System.out.println("Saliendo del sistema");
+                   break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+        }
+
+        sc.close();
+    }
+}
+    
