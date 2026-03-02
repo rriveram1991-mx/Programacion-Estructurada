f4262b1670122b075ffcff7dea434c54f59b50e1
Zoe Morales García <v0754437@gmail.com>
2026-03-02 17:35:48 -0600

    FOR-each & ARRAYS

---
+++ 2B TSU/Semana 5/Zoe Morales/Mavenproject30.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject30;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mavenproject30 {
+
+    public static void main(String[] args) {
+        String[] persona = {"Zoe", "Aldo", "Cathy", "Luis", "Milo"};
+        int[] edades = {18, 13, 46, 46, 49};
+        
+        for (String Persona: persona) {
+            System.out.println(persona);
+        }
+        for (int Edad: edades) {
+            System.out.println(edades);
+        }
+
+        
+    }
+}
