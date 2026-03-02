bdba87a1575577a5c2b97eb150331943b34ec690
Zoe Morales García <v0754437@gmail.com>
2026-03-02 17:43:53 -0600

    Add files via upload

---
+++ 2B TSU/Semana 5/Zoe Morales García/REQ.SEM5-2B(3).java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject30;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mavenproject30 {
+
+    public static void main(String[] args) {
+        int[] numeros = {2, 4, 6, 8, 10};
+        int suma = 0;
+        for (int num : numeros) {
+            System.out.println(num);
+            suma += num;
+        }
+
+        System.out.println("La suma total es: " + suma);
+
+
+        
+    }
+}
