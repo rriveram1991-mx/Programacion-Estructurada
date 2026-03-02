5495263cf49a73c0f3b1e1b2ca3b061dd2c115dc
emlarck <emilianolaradgz90@gmail.com>
2026-03-02 10:55:17 -0600

    Add files via upload

---
+++ 2B TSU/Semana 5/Emiliano Lara/2B TSU/REQ.SEM5-2B.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package array1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emili
+ */
+public class Array1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Scanner geo = new Scanner (System.in);
+//EJEMPLO 1
+        /*String[] n = {"Somalia","Chad","Malí","Angola","Mozambique"};
+System.out.println("Paises con mayor natalidad del mundo");
+for (String na : n){
+    System.out.println(""+na);
+}*/
+        //EJEMPLO 2
+/*System.out.println("Inserte sus calificaciones");
+        int[] hu= new int[6];
+        for(int i=0; i<hu.length; i++){
+            System.out.println("CAL");
+            hu[i]=geo.nextInt();   
+        }
+            for(int i=0; i<hu.length; i++){
+                System.out.println(hu[i]);          
+            }*/
+//EJEMPLO 3
+/*
+             int[] lad = {52,1,39,54,236};     
+for (int i:lad) {
+    System.out.println(""+i);
+}*/
+
+    }   
+}
