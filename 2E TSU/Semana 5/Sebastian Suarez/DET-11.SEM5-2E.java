1fa8da58eb6b5042487d2b082c665ea2ebe92d4e
Sebastian Suarez <sebastian26ju@gmail.com>
2026-03-04 20:38:27 -0600

    Add files via upload

---
+++ 2E TSU/Semana 5/Sebastian Suarez/DET-11.SEM5-2E.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SistemaCalificaciones {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] calificaciones = new int[5];
+        int suma = 0;
+        int aprobados = 0;
+
+        // Captura de datos
+        for (int i = 0; i < calificaciones.length; i++) {
+            System.out.println("Ingrese calificación " + (i+1) + ": ");
+            calificaciones[i] = sc.nextInt();
+
+            while (calificaciones[i] < 0 || calificaciones[i] > 100) {
+                System.out.println("Calificación inválida. Intente nuevamente:");
+                calificaciones[i] = sc.nextInt();
+            }
+        }
+
+        // Mostrar calificaciones
+        for (int cal : calificaciones) {
+            System.out.println("Calificación: " + cal);
+            suma += cal;
+        }
+
+        // Contar aprobados
+        int j = 0;
+        while (j < calificaciones.length) {
+            if (calificaciones[j] >= 70) {
+                aprobados++;
+            }
+            j++;
+        }
+
+        double promedio = (double) suma / calificaciones.length;
+
+        if (promedio >= 70)
+            System.out.println("Grupo aprobado");
+        else
+            System.out.println("Grupo reprobado");
+
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Aprobados: " + aprobados);
+        sc.close();
+    }
+
+}
