09df04ddbcc234723fea7b836ed82295b1464980
DarthErick23 <erick_zapata32@hotmail.com>
2026-03-04 22:27:35 -0600

    Add files via upload

---
+++ 2B TSU/Semana 5/Erick Tabares/REQSEM52B.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class SistemaCalificaciones 
+{
+
+    public static void main(String[] args) 
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] calificaciones = new int [5] ;
+        int suma = 0;
+        int aprobados = 0;
+
+        // Captura de datos
+
+      // Captura de datos
+    System.out.println("El promedio se calificará de (0 a 100)");
+    for (int i = 1; i < calificaciones.length; i++) 
+    {
+    System.out.println("Ingrese calificación : " + (i) + ": ");
+    calificaciones[i] = sc.nextInt();
+
+    if (calificaciones[i]>= 70) 
+            {
+                aprobados++ ;
+            
+            }
+
+    while (calificaciones[i] < 0 || calificaciones[i] > 100) 
+    {
+
+        System.out.println("Calificación inválida. Intente nuevamente : ");
+        System.out.println("Ingrese calificación : " + (i+1) + ": ");
+        calificaciones[i] = sc.nextInt();
+    }
+    }
+
+// Mostrar calificaciones y sumar
+    for (int cal : calificaciones) 
+        {
+    System.out.println("Calificación: " + cal);
+    suma += cal;
+}
+
+
+        
+
+        // Contar aprobados
+        
+
+        double promedio = suma / calificaciones.length;
+
+        if (promedio >= 70)
+        {
+            System.out.println("Grupo aprobado");
+        }    
+        else
+        {
+            System.out.println("Grupo reprobado");
+        }    
+
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Alumnos aprobados: " + aprobados);
+        
+        sc.close();
+    }
+    
+}
