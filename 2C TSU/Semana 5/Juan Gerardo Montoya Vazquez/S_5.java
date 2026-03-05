f80b9ee16f0470a3256d5a387a25c46a040a4e7f
gera1709 <juangerardomontoya35@gmail.com>
2026-03-04 22:03:21 -0600

    Add files via upload

---
+++ 2C TSU/Semana 5/Juan Gerardo Montoya Vazquez/S_5.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package s_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juang
+ */
+public class S_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner sc = new Scanner(System.in);
+
+        int[] calificaciones = new int[5];
+        int suma = 0;
+        int aprobados = 0;
+
+        // Captura de datos
+        for (int i = 0; i < calificaciones.length; i++) {
+
+            System.out.println("Ingrese calificación " + (i + 1) + ": ");
+            calificaciones[i] = sc.nextInt();
+
+            while (calificaciones[i] < 0 || calificaciones[i] > 100) {
+                System.out.println("Calificación inválida. Intente nuevamente:");
+                calificaciones[i] = sc.nextInt();
+            }
+        }
+
+        // Mostrar calificaciones y sumar
+        for (int cal : calificaciones) {
+            System.out.println("Calificación: " + cal);
+            suma += cal;
+        }
+
+        // Contar aprobados
+        for (int j = 0; j < calificaciones.length; j++) {
+            if (calificaciones[j] >= 70) {
+                aprobados++;
+            }
+        }
+
+        double promedio = (double) suma / calificaciones.length;
+
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Aprobados: " + aprobados);
+
+        if (promedio >= 70)
+            System.out.println("Grupo aprobado");
+        else
+            System.out.println("Grupo reprobado");
+
+        sc.close();
+    }
+}
