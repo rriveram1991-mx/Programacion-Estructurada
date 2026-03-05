e30e8dbaa7800e42e63894c1e365583fee72e64c
Vellito34 <151584119+Vellito34@users.noreply.github.com>
2026-03-04 21:22:27 -0600

    Add files via upload

---
+++ 2E TSU/Semana 5/Luis Alejandro Galvan Gonzalez/programa2.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class programa2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] ventas = new double[6];
+        double total = 0;
+        double mayor = 0;
+
+        // Captura de ventas
+        for (int i = 0; i < ventas.length; i++) {
+
+            System.out.println("Ingrese venta #" + (i + 1) + ": ");
+            ventas[i] = sc.nextDouble();
+
+            while (ventas[i] <= 0) {
+                System.out.println("No se permiten ventas negativas. Intente de nuevo:");
+                ventas[i] = sc.nextDouble();
+            }
+        }
+
+        // Calcular total y mayor
+        for (double v : ventas) {
+            total += v;
+
+            if (v > mayor) {
+                mayor = ventas[0];
+            }
+        }
+
+        double promedio = total / ventas.length;
+
+        // Contar ventas mayores al promedio
+        int contador = 0;
+        int j = 0;
+
+        while (j < ventas.length) {
+
+            if (ventas[j] > promedio) {
+                contador++;
+            }
+            j++;
+        }
+
+        if (contador > 0){
+            System.out.println("Hay ventas mayores al promedio");
+        }
+        System.out.println("Total vendido: " + total);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Venta mayor: " + mayor);
+        System.out.println("Cantidad mayores al promedio: " + contador);
+    }
+}
