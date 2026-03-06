61be5d7294c824bebb7b9b032a1fec4828df861a
SaidValenzuela <alejandrovalenzuela2543@gmail.com>
2026-03-05 21:26:35 -0600

    Add files via upload

---
+++ 2C TSU/Semana 5/S5 Said Alejandro Valenzuela Quiñones/S5_Pacientes.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package s5_pacientes;
+
+import java.util.Scanner;
+
+public class S5_Pacientes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Cme = 0, Ca = 0, Cam = 0, Cpp = 0;
+        
+        System.out.println("Ingresa la cantidad de pacientes:");
+        int pacientes = sc.nextInt();
+        sc.nextLine();
+        
+        String[] nombres = new String[pacientes];
+        int[] edades = new int[pacientes];
+        
+        for (int i = 0; i < pacientes; i++){
+            System.out.println("\n===== REGISTRO DEL PACIENTE " + (i + 1) + " =====");
+            System.out.print("Nombre del paciente: ");
+            nombres[i] = sc.nextLine();
+            
+            System.out.print("Edad del paciente: ");
+            int edad = sc.nextInt();
+            sc.nextLine();
+                      
+            while(edad < 0 || edad > 120){
+                System.out.print("Edad invalida, intente nuevamente: ");
+                edad = sc.nextInt();  
+                sc.nextLine();
+            }        
+            
+            // Contadores (edad)
+            if(edad < 18){
+                Cme++;
+            }
+            else if(edad <= 59){
+                Ca++;
+            }
+            else{
+                Cam++;
+                Cpp++;
+            }
+            edades[i] = edad;            
+        }
+        
+        // Mostrar pacientes
+        System.out.println("\n===== PACIENTES REGISTRADOS =====");
+        for (int i = 0; i < pacientes; i++){
+            if(edades[i] >= 60){
+                System.out.println("PACIENTE PRIORITARIO " + " Nombre: " + nombres[i] + " Edad: " + edades[i]);
+            }
+            else{
+                System.out.println("Nombre: " + nombres[i] + " Edad: " + edades[i]);
+            }
+        }
+        
+            System.out.println("\nMenores de edad: " + Cme);
+            System.out.println("Adultos: " + Ca);
+            System.out.println("Adultos mayores: " + Cam);
+            System.out.println("Pacientes prioritarios: " + Cpp);
+    }
+    
+}
